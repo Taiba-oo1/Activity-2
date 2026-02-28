09a5455f733d6c775027088a267760b4752a8d23
Taiba-oo1 <taibanaz3110@gmail.com>
2026-02-28 21:12:34 +0500

    Add files via upload

---
+++ activity2.asm
@@ -0,0 +1,31 @@
+ 
+
+.MODEL SMALL                  ; SMALL memory model (one code, one data)
+
+.STACK 100H                   ; Allocate stack of 256 bytes (100H)
+
+.DATA                          ; Start of DATA segment
+MSG DB 'ENTER A CHARACTER: $'  ; Message string ending with '$'
+
+.CODE                          ; Start of CODE segment
+MAIN PROC                     ; MAIN procedure begins (entry point)
+
+    MOV AX, @DATA              ; Move DATA segment address into AX
+    MOV DS, AX                 ; Move AX value into DS (initialize data segment)
+
+    MOV AH, 09H                ; AH = 09H (DOS function to display string)
+    LEA DX, MSG                ; Load Effective Address of MSG into DX
+    INT 21H                    ; Call DOS interrupt to display message
+
+    MOV AH, 01H                ; AH = 01H (DOS function to input character)
+    INT 21H                    ; Read character from keyboard into AL
+
+    MOV DL, AL                 ; Move input character from AL to DL
+    MOV AH, 02H                ; AH = 02H (DOS function to display character)
+    INT 21H                    ; Display character on screen
+
+    MOV AH, 4CH                ; AH = 4CH (DOS function to terminate program)
+    INT 21H                    ; Terminate program execution
+
+MAIN ENDP                     ; End of MAIN procedure
+END MAIN                      ; End of program
